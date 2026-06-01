14c232909eb2165484de3cb401782fe3fd8727a7
deonugroho <deonugroz888@gmail.com>
2026-06-01 19:17:44 +0700

    Membuat perulangan untuk memunculkan setiap baris

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -26,4 +26,17 @@ int main()
     
     ifstream infile;
     infile.open("contohfile.txt");
+    
+    cout << endl << ">=Membuka dan membaca file " << endl;
+   
+    if (infile.is_open())
+    {
+        while (getline(infile, baris)) {
+            cout << baris << endl;
+        }
+        infile.close();
+    }
+    else cout << "Unable to open file";
+    return 0;
+    
 }
