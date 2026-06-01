2028e6f4d78d3fbe623e050bde57ca66f5007919
deonugroho <deonugroz888@gmail.com>
2026-06-01 19:43:38 +0700

    Kondisi jika terjadi error di dalam try

--- ExceptionStandart.cpp
+++ ExceptionStandart.cpp
@@ -12,4 +12,9 @@ int main()
         array <int, 3> data = { 1, 2, 3 };
         cout << data.at(5) << endl;
     }
+    catch (exception& e)
+    {
+        
+        cout << e.what() << endl;
+    }
 }
